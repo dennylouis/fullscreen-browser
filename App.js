f12024f8ce10a4cb2bee9f3f4b70597aa905d835
Denny Louis <dennylouis@dennys-mbp.home>
2019-08-03 14:40:10 +1000

    .

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { View, StatusBar, WebView } from "react-native";
+import { StyleSheet, StatusBar, WebView } from "react-native";
 
 export default function App() {
   return (
-    <View>
+    <>
       <StatusBar hidden />
       <WebView
+        style={styles.webView}
         source={{
           uri:
             "https://www.figma.com/proto/O3b3pjNXO2lzSalUzafCnLJQ/AO-App?node-id=376%3A626&viewport=143%2C643%2C0.015625&scaling=scale-down"
         }}
       />
-    </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  webView: {
+    flex: 1,
+    backgroundColor: "red"
+  }
+});
