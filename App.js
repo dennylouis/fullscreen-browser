13e2556484e850b87942938606578e83aec2f189
Denny Louis <dennylouis@dennys-mbp.home>
2019-08-04 12:44:40 +1000

    .

--- App.js
+++ App.js
@@ -1,41 +1,39 @@
 import React, { useState } from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, ActivityIndicator } from "react-native";
 import styled from "styled-components/native";
 
 export default function App() {
-  const uri =
-    "https://www.figma.com/proto/O3b3pjNXO2lzSalUzafCnLJQ/AO-App?node-id=376%3A626&viewport=143%2C643%2C0.015625&scaling=scale-down";
-
-  const [url, setUrl] = useState("");
+  const [uri, setUri] = useState("");
   const [showWebView, setShowWebView] = useState(false);
   const [showLoader, setShowLoader] = useState(false);
 
   return (
     <Container>
+      <StatusBar hidden={showWebView && !showLoader} />
       {!showWebView && (
         <SafeAreaView>
-          <StatusBar />
           <TextInput
-            onChangeText={text => setUrl(text)}
-            value={url}
+            onChangeText={text => setUri(text)}
+            value={uri}
             onSubmitEditing={() => setShowWebView(true)}
-            keyboardType="url"
+            keyboardType="web-search"
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Enter URL here"
           />
         </SafeAreaView>
       )}
-      {showLoader && <Loader />}
+      {showLoader && (
+        <Overlay>
+          <ActivityIndicator />
+        </Overlay>
+      )}
       {showWebView && (
-        <>
-          <StatusBar hidden />
-          <WebView
-            source={{ uri }}
-            onLoadStart={() => setShowLoader(true)}
-            onLoadEnd={() => setShowLoader(false)}
-          />
-        </>
+        <WebView
+          source={{ uri }}
+          onLoadStart={() => setShowLoader(true)}
+          onLoadEnd={() => setShowLoader(false)}
+        />
       )}
     </Container>
   );
@@ -46,6 +44,22 @@ const Container = styled.View`
   align-items: center;
   justify-content: center;
   background-color: white;
+  height: 100%;
+  width: 100%;
+  flex-direction: row;
+`;
+
+const Overlay = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 100;
 `;
 
 const SafeAreaView = styled.SafeAreaView`
@@ -61,7 +75,6 @@ const TextInput = styled.TextInput`
   height: 48px;
   padding: 12px;
   font-size: 16px;
-  /* line-height: 24px; */
   flex: 1;
   margin: 16px;
   border-radius: 4px;
@@ -69,14 +82,7 @@ const TextInput = styled.TextInput`
 
 const WebView = styled.WebView`
   flex: 1;
-  background-color: red;
   height: 100%;
   width: 100%;
-`;
-
-const Loader = styled.View`
-  height: 40px;
-  width: 40px;
-  border-radius: 20px;
-  background-color: red;
+  overflow: hidden;
 `;
